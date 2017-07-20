2375268c0612083c31f0f7ee7e680ef0d9bb8477
DNavarro10 <luis_d_100@hotmail.com>
2017-07-19 19:07:37 -0500

    DB y regristrate.php
    Agregados y pendiente encriptar?

--- Vistas/registrate.view.php
+++ Vistas/registrate.view.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+	<script src="https://use.fontawesome.com/323a076c8b.js"></script>
 	<link rel="stylesheet" href="CSS/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 	<title>Registro</title>
@@ -23,6 +23,13 @@
 					<div class="form-group">
 						<i class="icono izquierda fa fa-lock"></i><input type="password" name="password2" class="password_btn" placeholder="Repetir contraseña"><i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
 					</div>
+					<?php if(!empty($errores)): ?>
+						<div class="error">
+							<ul>
+								<?php echo $errores; ?>	
+							</ul>
+						</div>
+					<?php endif; ?>
 				</form>	
 			</div>
 			<p class="texto-registrate">
